2c3e714e6d1d05f63081eac9cd084b403bb008fc
Yayufan <kamikazey2200@gmail.com>
2025-07-24 14:33:50 +0800

    update agenda
    - 更新議程

--- pages/index.vue
+++ pages/index.vue
@@ -140,7 +140,7 @@
                 </div>
 
                 <div style="width: 80%;" >
-                    <img style="width: 100%;"  src="/img/agenda.jpg">
+                    <img style="width: 100%;"  src="/img/agenda.png">
                 </div>
 
             </div>
